cdb0ad3507944852ccc2527bfaa42d60d8ca53e0
Qurban Ali <qurbanaliqureshi@gmail.com>
2021-12-28 00:55:28 +0000

    basic register form added

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("You are at blog's index.")
+
+def register(request):
+    if request.method == "POST":
+        return HttpResponse(request.POST.get("username"))
+    else:
+        return render(request, "blog/register.html", context={})
