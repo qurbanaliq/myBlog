9ca7970ec9866acf837c4c7427fdbd9abb8554eb
Qurban Ali <qurbanaliqureshi@gmail.com>
2022-01-01 01:26:42 +0000

    basic registeration created

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login as _login, logout as _logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import Q
 
 @login_required
 def index(request):
@@ -16,7 +18,22 @@ def register(request):
     """shows the registration form to the user and registers the user
     """
     if request.method == "POST":
-        return HttpResponse(request.POST.get("username"))
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        password = request.POST.get("password")
+        # create user when it doesn't already exist
+        if not User.objects.filter(Q(username=username) | Q(email=email)):
+            if password:
+                User.objects.create_user(username=username, email=email,
+                    first_name=first_name, last_name=last_name,
+                    password=password)
+                return redirect(reverse("blog:login"))
+            return render(request, "blog/register.html",
+                context={"error": "Password field can't be empty."})
+        return render(request, "blog/register.html",
+            context={"error": "User already registered."})
     return render(request, "blog/register.html", context={})
 
 def login(request):
@@ -35,7 +52,8 @@ def login(request):
                         return redirect(nxt)
                     return redirect(reverse("blog:index"))
                 return HttpResponse("Inactive user")
-            return render(request, "blog/login.html", context={"error": "Wrong credidentials"})
+            return render(request, "blog/login.html",
+                context={"error": "Wrong credidentials"})
         return render(request, "blog/login.html", context={})
     return redirect("/blog")
 
